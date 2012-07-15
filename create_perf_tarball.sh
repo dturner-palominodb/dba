0e8984f85fc8ac9eeb0f22ecabe9a231e82b5392
David Turner <dturner@palominodb.com>
2012-07-15 15:10:53 -0700

    New script for creating the perf_tarball.

---
+++ create_perf_tarball.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+tarball=/tmp/perf_tarball.tar
+
+cd ${HOME}/git/ServerAudit/scripts/
+tar cvf ${tarball} \
+call_pt-stalk.sh \
+gen_stalk_report.sh
+
+cd ${HOME}/git/dba/
+tar rvf ${tarball} \
+functions \
+mysqld \
+network \
+vfa_lib.sh 
+
+gzip ${tarball}
+
+
