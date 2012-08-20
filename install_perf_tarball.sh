60cd45976ba29fba8aec2e7882a4540f94a3a395
dturner-palominodb <dturner@palominodb.com>
2012-08-20 15:52:58 -0700

    Update install_perf_tarball.sh
    Updated to find mysql instances via my.cnf files.

--- install_perf_tarball.sh
+++ install_perf_tarball.sh
@@ -3,11 +3,27 @@
 dest_dir=/usr/local/palominodb/scripts
 tarball=/tmp/perf_tarball.tar
 tarball_dir=/tmp/perf_tarball
+mysql_conf_dir=/etc/mysql
 
 mkdir -p ${dest_dir} 2> /dev/null
 cd ${dest_dir}
 
 cp ${tarball_dir}/* ${dest_dir}
 
-echo "Install complete."
+if [ `ls ${mysql_conf_dir}/my-*33*.cnf 2> /dev/null |wc -l` -lt 1 ];then
+  if [ -d /data/admin/conf/ ];then
+    mysql_conf_dir="/data/admin/conf"
+  else
+    echo "Error: problem finding my.cnf files in ${mysql_conf_dir}"
+    exit 1
+  fi
+fi
 
+for conf in `ls ${mysql_conf_dir}/my-*33*.cnf |sort`
+do
+  echo $conf:$(echo $conf |awk -F"/" '{print $NF}'|sed "s/my-m//;s/.cnf//"):Y:N
+done > ${mysql_conf_dir}/vfatab
+ln -s ${mysql_conf_dir}/vfatab /etc/vfatab
+
+
+echo "Install complete."
