9dd3da42e26d8830f8f24c21c600c56062699edf
dturner-palominodb <dturner@palominodb.com>
2012-08-21 14:10:18 -0700

    Update install_perf_tarball.sh
    Added checks for symlinks and added alias to .bashrc

--- install_perf_tarball.sh
+++ install_perf_tarball.sh
@@ -23,18 +23,38 @@ for conf in `ls ${mysql_conf_dir}/my-*33*.cnf |sort`
 do
   echo $conf:$(echo $conf |awk -F"/" '{print $NF}'|sed "s/my-m//;s/.cnf//"):Y:N
 done > ${mysql_conf_dir}/vfatab
+
+rm -f /etc/vfatab
 ln -s ${mysql_conf_dir}/vfatab /etc/vfatab
 
 if [ -e /etc/init.d/mysql ];then
-  mv /etc/init.d/mysql /etc/init.d/mysql.old
+  if [ -h /etc/init.d/mysql ];then
+    rm -f /etc/init.d/mysql
+  else
+    mv /etc/init.d/mysql /etc/init.d/mysql.old
+  fi
 fi
 
 if [ -e /etc/init.d/mysqld ];then
-  mv /etc/init.d/mysqld /etc/init.d/mysqld.old
+  if [ -h /etc/init.d/mysqld ];then
+    rm -f /etc/init.d/mysqld
+  else
+    mv /etc/init.d/mysqld /etc/init.d/mysqld.old
+  fi
 fi
 
 ln -s /usr/local/palominodb/scripts/mysqld /etc/init.d/mysql
 ln -s /usr/local/palominodb/scripts/mysqld /etc/init.d/mysqld
 
+# Check for bashrc and add dba alias if it doesn't already exist
+
+if [ -e ${HOME}/.bashrc ];then
+  if [ `grep "alias dba" ${HOME}/.bashrc | wc -l` -lt 1 ];then
+    echo >> ${HOME}/.bashrc
+    echo "# Added by palominodb for vfa_lib.sh `date`" >> ${HOME}/.bashrc
+    echo "alias dba=\"source ${dest_dir}/vfa_lib.sh\"" >> ${HOME}/.bashrc
+
+  fi
+fi
 
 echo "Install complete."
