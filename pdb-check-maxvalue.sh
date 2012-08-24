6d227baeb2e304f1976d2ab827b1393530251de8
David Turner <dturner@palominodb.com>
2012-08-24 11:47:52 -0500

    More data

--- pdb-check-maxvalue.sh
+++ pdb-check-maxvalue.sh
@@ -14,16 +14,14 @@ if [ -z $1 ];then
   exit 1
 else
  pct_allowed=`echo $1 | sed s/%//g`
- echo "pct_allowed=${pct_allowed}"
+
 fi
-echo "2=$2"
+
 if [ -z $2 ];then
   port=3306
 else
   port=$2
 fi
-
-echo port=$port
 
 if [ -e ${vfa_lib_file} ];then
   source ${vfa_lib_file} ''
@@ -67,7 +65,35 @@ select
     ELSE
       'failed'
   END),
-  ' * 100 )) as PCT_USED from \`', table_schema, '\`.\`', table_name, '\` '
+  ' * 100 )', 
+  ', round(ifnull(max(\`', column_name, '\`),0)),',
+  (CASE
+      1
+    WHEN
+      column_type regexp '^tinyint\\\([0-9]*\\\)$'          THEN ~0 >> 57 #tiny   int signed
+    WHEN
+      column_type regexp '^tinyint\\\([0-9]*\\\) unsigned$' THEN ~0 >> 56 #tiny   int unsigned
+    WHEN
+      column_type regexp '^smallint\\\([0-9]*\\\)$'          THEN ~0 >> 49 #small  int signed
+    WHEN
+      column_type regexp '^smallint\\\([0-9]*\\\) unsigned$' THEN ~0 >> 48 #small  int unsigned
+    WHEN
+      column_type regexp '^mediumint\\\([0-9]*\\\)$'          THEN ~0 >> 41 #medium int signed
+    WHEN
+      column_type regexp '^mediumint\\\([0-9]*\\\) unsigned$' THEN ~0 >> 40 #medium int unsigned
+    WHEN
+      column_type regexp '^int\\\([0-9]*\\\)$'          THEN ~0 >> 33 #       int signed
+    WHEN
+      column_type regexp '^int\\\([0-9]*\\\) unsigned$' THEN ~0 >> 32 #       int unsigned
+    WHEN
+      column_type regexp '^bigint\\\([0-9]*\\\)$'          THEN ~0 >>  1 #big    int signed
+    WHEN
+      column_type regexp '^bigint\\\([0-9]*\\\) unsigned$' THEN ~0       #big    int unsigned
+    ELSE
+      'failed'
+  END),
+  ') as INFO ',
+  'from \`', table_schema, '\`.\`', table_name, '\` '
   'having round(ifnull(max(\`', column_name, '\`),0) / ',
   (CASE
       1
@@ -101,14 +127,13 @@ from
 where 
   # data_type in ('tinyint')
   data_type in ('tinyint','smallint','mediumint','int','integer','bigint')
-and 
-  table_schema not in ('blah');
+and
+  table_schema not in ('VALUE WILL BE AN OPTION IN FUTURE VERSION. HARD CODE IF NECESSARY');
+  # table_schema not in ('mysql','information_schema','VALUE WILL BE AN OPTION IN FUTURE VERSION. HARD CODE IF NECESSARY');
 
 EOF
 
-echo "set @myvar='this works';" > ${proc_file}
-
-mysql -sN ${socket_info} < ${sql_file} >> ${proc_file}
+mysql -sN ${socket_info} < ${sql_file} > ${proc_file}
 
 # For debugging
 # cat ${proc_file}
