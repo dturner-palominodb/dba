3070c77d6d1e6f1222e7e04234946ae9477601b4
David Turner <dturner@palominodb.com>
2012-09-29 11:09:41 -0700

    Added files to be copied to tarball.

--- create_perf_tarball.sh
+++ create_perf_tarball.sh
@@ -12,16 +12,22 @@ mkdir -p /tmp/perf_tarball 2> /dev/null
 # cp gen_stalk_report.sh ${tarball_dir}
 
 cd ${HOME}/git/dba/
-cp functions                  ${tarball_dir}
-cp mysqld                     ${tarball_dir}
-cp vfa_lib.sh                 ${tarball_dir}
-cp install_perf_tarball.sh    ${tarball_dir}
-cp pdb_checksum_to_file.sh    ${tarball_dir}
-cp pdb_dump.sh                ${tarball_dir}
-cp pdb_set_long_query_time.sh ${tarball_dir}
-cp pdb-check-maxvalue.sh      ${tarball_dir}
+cleanup_osc.sh
+cp functions                       ${tarball_dir}
+cp install_perf_tarball.sh         ${tarball_dir}
+cp mysqld                          ${tarball_dir}
+cp pdb-check-maxvalue.sh           ${tarball_dir}
+cp pdb-defrag.sh                   ${tarball_dir}
+cp pdb-local-reorg.sh              ${tarball_dir}
+cp pdb-track-table-usage.sh        ${tarball_dir}
+cp pdb-track-user-stats.sh         ${tarball_dir}
+cp pdb_checksum_to_file.sh         ${tarball_dir}
+cp pdb_checksum_to_file.sh         ${tarball_dir}
+cp pdb_dump.sh                     ${tarball_dir}
+cp pdb_set_long_query_time.sh      ${tarball_dir}
+cp replace_mysql_startup_script.sh ${tarball_dir}
+cp vfa_lib.sh                      ${tarball_dir}
 
 cd /tmp
 tar czvf ${tarball_file} perf_tarball
-
 
