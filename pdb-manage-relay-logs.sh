7b3896075c4b35e55147213e2e123c9cd20f6341
dturner-palominodb <dturner@palominodb.com>
2013-01-30 07:56:22 -0800

    Update pdb-manage-relay-logs.sh
    Added more reporting of state.

--- pdb-manage-relay-logs.sh
+++ pdb-manage-relay-logs.sh
@@ -6,7 +6,6 @@
 #          or is nolonger having an issue with filling the disk.
 # note: The script was written in haste and needs the hard coding removed and other cleanup.
 #
-
 logfile=pdb-manage-relay-logs.log
 
 slave_status=`echo $(mysql -e 'show slave status\G' |egrep -i "runn|sec")|awk '{print $2 "," $4 "," $6}'`
@@ -19,7 +18,6 @@ exec > >(tee -a $logfile) 2>&1
 echo slave_status=$slave_status
 echo pct_disk_used=$pct_disk_used
 
-
 while [ $sbm == "NULL" ] || [ $sbm -gt 1000 ]
 do
 
@@ -27,21 +25,23 @@ do
     echo "Stopping io_thread"
     mysql -e 'stop slave io_thread'
     echo $(mysql -e 'show slave status\G' |egrep -i "runn|sec")
+    df -k |grep sda2
+    date
   else
     if [ ${pct_disk_used} -lt 36 ];then
       echo "Starting io_thread"
       mysql -e 'start slave io_thread'
       echo $(mysql -e 'show slave status\G' |egrep -i "runn|sec")
-
+      df -k |grep sda2
+      date
     fi
-
+    echo $(mysql -e 'show slave status\G' |egrep -i "runn|sec")
+    df -k |grep sda2
+    date
   fi
-
-  date
 
   slave_status=`echo $(mysql -e 'show slave status\G' |egrep -i "runn|sec")|awk '{print $2 "," $4 "," $6}'`
   sbm=`echo $slave_status | cut -d, -f3`
-
   pct_disk_used=`df -Ph |tail -n +2 |grep sda2|awk '{print $5}' |sed "s/%//"`
 
   sleep 120
