8b648fd4d598e61201b7be34b9f4bc26eb0c8295
David Turner <dturner@palominodb.com>
2013-02-11 07:34:23 -0800

    Added feature request from Emanuel

--- pdb_dump.py
+++ pdb_dump.py
@@ -36,6 +36,7 @@ from pdb_dba import *
 #    o compare start and end slave 
 #    o compare start and end master 
 # Addtional features
+#    o Add exit codes with useful numbers(Work with Emanuel)
 #    o Better logging/handling of failures
 #    o Read user and pass from defaults-file
 #    o Accept user and pass on command line
