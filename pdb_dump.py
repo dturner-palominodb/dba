069d5e650f313fc06046721aecbadc3ee8b301d4
David Turner <dturner@palominodb.com>
2013-02-10 22:18:27 -0800

    Added steps to ensure solid dumps.

--- pdb_dump.py
+++ pdb_dump.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # purpose perform a parallel backup of databases.
 #
+import filecmp
 import os
 import sys
 import subprocess
+import time
 from optparse import OptionParser
 import grp, pwd
 from pwd import getpwnam
@@ -19,14 +21,31 @@ from pdb_dba import *
 # time mysql -e 'insert into shard01.voice_calls select * from shard03.voice_calls; insert into shard03.voice_calls select * from shard01.voice_calls '
 #
 # features I would like to add:
+#
+#    x store pt-show-grants in a file for later loading.
+#    x put in read only
+#    o flush logs
+#    x stop replication 
+#    x store starting slave position
+#    o remove all grants except for those that were used to log into the db.
+#    x store starting master position.
 #    o As dumps complete add dump processes to the queue
 #    o Add and option to perform parallel checksums of tables to a file, sorting after for an easy diff on import.
+#    x store ending slave posiition.
+#    x store ending master posiition.
+#    o compare start and end slave 
+#    o compare start and end master 
+# Addtional features
 #    o Better logging/handling of failures
 #    o Read user and pass from defaults-file
 #    o Accept user and pass on command line
 #    o The script should estimate completion time and show progress.
 #    o Support views, triggers, stored proc, etc (low pri)
-#
+#    o Support running this script remotely.
+#    
+# Import
+#    o Modify my.cnf and restart db.
+
 
 # Globals
 mysql_user="root"
@@ -113,19 +132,23 @@ def dump_tables(inst_host, inst_port, mysql_user, mysql_pass, table_list, backup
         db = table.split(".")[0]
         table_name = table.split(".")[1]
 
+        # DEBUG this needs to handle passwords.
         cmd = "mysqldump -h " + inst_host + " -u " + mysql_user + " --socket=" + get_socket(inst_port) + " -q -Q -e --no-data " +  db + " " + table_name + " > " + backup_dir + "/" + db + "/" + table_name + ".sql"
 
         proc = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
         return_code = proc.wait()
    
+        if return_code > 0:
+            for line in proc.stderr:
+                print ("stderr: " + line.rstrip())
+            sys.exit(1)
+
         for line in proc.stdout:
             print line.rstrip()
-            # return line.rstrip()
         for line in proc.stderr:
             print ("stderr: " + line.rstrip())
 
     # Perform the data dump    
-
     parallel_count = 0
     
     for table in table_list:
@@ -150,11 +173,31 @@ def dump_tables(inst_host, inst_port, mysql_user, mysql_pass, table_list, backup
                 else:
                     parallel_count = 0
 
+def call_pt_show_grants(inst_host, inst_port, mysql_user, mysql_pass, backup_dir):
+    # When you have time make sure this works with ports as well as sockets.
+    # Get this handling blank passwords
+    cmd = "pt-show-grants -h " + inst_host + " -u " + mysql_user + " --socket=" + get_socket(inst_port) + " > " + backup_dir + "/" + "pt_show_grants.sql"
+   
+    print cmd  
+
+    proc = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    return_code = proc.wait()
+  
+    if return_code > 0:
+        for line in proc.stderr:
+            print ("stderr: " + line.rstrip())
+        sys.exit(1)
+
+    for line in proc.stdout:
+        print line.rstrip()
+    for line in proc.stderr:
+        print ("stderr: " + line.rstrip())
+     
+
 def main():
     (options, args) = parse()
     # print options
     # print args 
-
   
     inst = options.inst 
     (inst_host, inst_port) = parse_inst_info(inst)
@@ -173,17 +216,89 @@ def main():
         print "Error: unable to connect to the instance. Check to confirm that it is available."
         sys.exit(1)
 
-    # stop replication
+    # set read_only
+    if set_read_only(host='localhost', port=3306, user='root', password='') == 1:
+        print "Instance is in read_only mode."
+    else:
+        print "Error: problem enabling read_only."
+        sys.exit(1)
 
     # flush logs
 
+    # stop replication - needs to return 1 or 0
+    stop_slave(inst_host, inst_port, mysql_user, mysql_pass)
+
+    # store master status
+    master_status_start_file = backup_dir + "/" + "master_status_start.txt"
+    fo = open(master_status_start_file,'w')
+    result = show_master_status(inst_host, inst_port, mysql_user, mysql_pass)
+    for row in result:
+        fo.write(
+            row[0] + ',' + 
+            str(row[1]) + ',' +
+            str(row[2]) + ',' +
+            str(row[3]) + 
+            '\n')
+    fo.close
+
+    # store slave status 
+    slave_status_start_file = backup_dir + "/" + "slave_status_start.txt"
+    fo = open(slave_status_start_file,'w')
+    result = show_slave_status(inst_host, inst_port, mysql_user, mysql_pass)
+    # This may be prone to errors. Take some time later to examine values that may come back as ints or null. May want to cast all to str to avoid
+    # wasting time.
+    for row in result:
+        pass
+        fo.write(
+            row[0] + ',' +
+            row[1] + ',' +
+            row[2] + ',' +
+            str(row[3]) + ',' +
+            str(row[4]) + ',' +
+            row[5] + ',' +
+            str(row[6]) + ',' +
+            row[7] + ',' +
+            str(row[8]) + ',' +
+            row[9] + ',' +
+            row[10] + ',' +
+            row[11] + ',' +
+            row[12] + ',' +
+            row[13] + ',' +
+            row[14] + ',' +
+            row[15] + ',' +
+            row[16] + ',' +
+            row[17] + ',' +
+            str(row[18]) + ',' +
+            row[19] + ',' +
+            str(row[20]) + ',' +
+            str(row[21]) + ',' +
+            str(row[22]) + ',' +
+            row[23] + ',' +
+            row[24] + ',' +
+            str(row[25]) + ',' +
+            row[26] + ',' +
+            row[27] + ',' +
+            row[28] + ',' +
+            row[29] + ',' +
+            row[30] + ',' +
+            row[31] + ',' +
+            str(row[32]) + ',' +
+            row[33] + ',' +
+            str(row[34]) + ',' +
+            row[35] + ',' +
+            str(row[36]) + ',' +
+            row[37] +
+            '\n'
+                )
+    fo.close
+
     # use pt-show-grants to get mysql privs
-    
+    call_pt_show_grants(inst_host, inst_port, mysql_user, mysql_pass, backup_dir)
+
+
+    # get list of databases    
     stmt = "select schema_name from information_schema.schemata where schema_name not in ('information_schema','performance_schema','mysql') and schema_name not like '#%'"
     db_list = run_select(inst_host, inst_port, mysql_user, mysql_pass, stmt)
-
-    # for row in result:
-    #     print row[0]
 
     create_backup_dirs_for_dbs(db_list, unix_user, backup_dir)
 
@@ -195,6 +310,84 @@ def main():
     dump_tables(inst_host, inst_port, mysql_user, mysql_pass, table_list, backup_dir, parallelism)
     
     
+    # store ending master status
+    master_status_end_file = backup_dir + "/" + "master_status_end.txt"
+    fo = open(master_status_end_file,'w')
+    result = show_master_status(inst_host, inst_port, mysql_user, mysql_pass)
+    for row in result:
+        fo.write(
+            row[0] + ',' + 
+            str(row[1]) + ',' +
+            str(row[2]) + ',' +
+            str(row[3]) + 
+            '\n')
+    fo.close
+
+    # store ending slave status 
+    slave_status_end_file = backup_dir + "/" + "slave_status_end.txt"
+    fo = open(slave_status_end_file,'w')
+    result = show_slave_status(inst_host, inst_port, mysql_user, mysql_pass)
+    # This may be prone to errors. Take some time later to examine values that may come back as ints or null. May want to cast all to str to avoid
+    # wasting time.
+    for row in result:
+        pass
+        fo.write(
+            row[0] + ',' +
+            row[1] + ',' +
+            row[2] + ',' +
+            str(row[3]) + ',' +
+            str(row[4]) + ',' +
+            row[5] + ',' +
+            str(row[6]) + ',' +
+            row[7] + ',' +
+            str(row[8]) + ',' +
+            row[9] + ',' +
+            row[10] + ',' +
+            row[11] + ',' +
+            row[12] + ',' +
+            row[13] + ',' +
+            row[14] + ',' +
+            row[15] + ',' +
+            row[16] + ',' +
+            row[17] + ',' +
+            str(row[18]) + ',' +
+            row[19] + ',' +
+            str(row[20]) + ',' +
+            str(row[21]) + ',' +
+            str(row[22]) + ',' +
+            row[23] + ',' +
+            row[24] + ',' +
+            str(row[25]) + ',' +
+            row[26] + ',' +
+            row[27] + ',' +
+            row[28] + ',' +
+            row[29] + ',' +
+            row[30] + ',' +
+            row[31] + ',' +
+            str(row[32]) + ',' +
+            row[33] + ',' +
+            str(row[34]) + ',' +
+            row[35] + ',' +
+            str(row[36]) + ',' +
+            row[37] +
+            '\n'
+                )
+    fo.close
+
+    # Hack for now. st_size was not registering the file
+    # size. Reopens for reads then closes.
+    fo = open(slave_status_end_file,'r')
+    fo.close
+
+    if filecmp.cmp(slave_status_start_file, slave_status_end_file):
+        print "OK, slave status unchanged." 
+    else:
+        print "Warning, slave status changed. See slave start and end files for differences." 
+
+    if filecmp.cmp(master_status_start_file, master_status_end_file):
+        print "OK, master status unchanged." 
+    else:
+        print "Warning, master status changed. See master start and end files for differences." 
 
 # call main 
 if __name__ == '__main__':
